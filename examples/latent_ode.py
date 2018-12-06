b1814d474742866c49e73671e48ad9bbaaa6d497
Xuechen <12689993+lxuechen@users.noreply.github.com>
2018-12-06 16:51:26 -0500

    clean up.

--- examples/latent_ode.py
+++ examples/latent_ode.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 import numpy.random as npr
 import matplotlib
-matplotlib.use("agg")
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -16,9 +16,10 @@ import torch.nn.functional as F
 parser = argparse.ArgumentParser()
 parser.add_argument('--adjoint', type=eval, default=False)
 parser.add_argument('--visualize', type=eval, default=False)
-parser.add_argument('--niters', type=int, default=500)
+parser.add_argument('--niters', type=int, default=2000)
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--train_dir', type=str, default=None)
 args = parser.parse_args()
 
 if args.adjoint:
@@ -34,7 +35,8 @@ def generate_spiral2d(nspiral=1000,
                       stop=1,  # approximately equal to 6pi
                       noise_std=.1,
                       a=0.,
-                      b=1.):
+                      b=1.,
+                      savefig=True):
   """Parametric formula for 2d spiral is `r = a + b * theta`.
 
   Args:
@@ -45,6 +47,7 @@ def generate_spiral2d(nspiral=1000,
     stop: spiral ending theta value
     noise_std: observation noise standard deviation
     a, b: parameters of the Archimedean spiral
+    savefig: plot the ground truth for sanity check
 
   Returns: 
     Tuple where first element is true trajectory of size (nspiral, ntotal, 2),
@@ -54,30 +57,39 @@ def generate_spiral2d(nspiral=1000,
   """
 
   # add 1 all timestamps to avoid division by 0
-  orig_ts = np.linspace(start, stop, num=ntotal) + 1.
+  orig_ts = np.linspace(start, stop, num=ntotal)
   samp_ts = orig_ts[:nsample]
 
-  # trivial time-invariant latent dynamics
-  zs = orig_ts
-
   # generate clock-wise and counter clock-wise spirals in observation space
-  def polar_to_traj(_rs):
-    xs, ys = _rs * np.cos(zs), _rs * np.sin(zs)
-    return np.stack((xs, ys), axis=1)
+  # with two sets of time-invariant latent dynamics
+  zs_cw = stop + 1. - orig_ts
+  rs_cw = a + b * 50. / zs_cw
+  xs, ys = rs_cw * np.cos(zs_cw) - 5., rs_cw * np.sin(zs_cw)
+  orig_traj_cw = np.stack((xs, ys), axis=1)
 
-  rs_cw = a + b / zs
-  rw_cc = a + b * zs
-  orig_traj_cw = polar_to_traj(rs_cw)
-  orig_traj_cc = polar_to_traj(rw_cc)
+  zs_cc = orig_ts
+  rw_cc = a + b * zs_cc
+  xs, ys = rw_cc * np.cos(zs_cc) + 5., rw_cc * np.sin(zs_cc)
+  orig_traj_cc = np.stack((xs, ys), axis=1)
+
+  if savefig:
+    plt.figure()
+    plt.plot(orig_traj_cw[:, 0], orig_traj_cw[:, 1], label='clock')
+    plt.plot(orig_traj_cc[:, 0], orig_traj_cc[:, 1], label='counter clock')
+    plt.legend()
+    plt.savefig('./ground_truth.png', dpi=500)
+    print('Saved ground truth spiral at {}'.format('./ground_truth.png'))
 
   # sample starting timestamps
   orig_trajs = []
   samp_trajs = []
   for _ in range(nspiral):
-    t0_idx = npr.multinomial(1, [1. / (ntotal - nsample)] * (ntotal - nsample))
-    t0_idx = np.argmax(t0_idx)
+    # don't sample t0 very near the start or the end
+    t0_idx = npr.multinomial(
+        1, [1. / (ntotal - 2. * nsample)] * (ntotal - int(2 * nsample)))
+    t0_idx = np.argmax(t0_idx) + nsample
 
-    cc = bool(npr.rand() > .5)  # select counter clock-wise
+    cc = bool(npr.rand() > .5)  # uniformly select rotation
     orig_traj = orig_traj_cc if cc else orig_traj_cw
     orig_trajs.append(orig_traj)
 
@@ -189,10 +201,10 @@ if __name__ == '__main__':
   obs_dim = 2
   nspiral = 1000
   start = 0.
-  stop = 8 * np.pi
+  stop = 6 * np.pi
   noise_std = .3
   a = 0.
-  b = 1.
+  b = .3
   ntotal = 1000
   nsample = 100
   device = torch.device('cuda:' + str(args.gpu)
@@ -219,34 +231,67 @@ if __name__ == '__main__':
   optimizer = optim.Adam(params, lr=args.lr)
   loss_meter = RunningAverageMeter()
 
-  for itr in range(1, args.niters + 1):
-    optimizer.zero_grad()
-    # backward in time to infer q(z_0)
-    h = rec.initHidden().to(device)
-    for t in reversed(range(samp_trajs.size(1))):
-      obs = samp_trajs[:, t, :]
-      out, h = rec.forward(obs, h)
-    qz0_mean, qz0_logvar = out[:, :latent_dim], out[:, latent_dim:]
-    epsilon = torch.randn(qz0_mean.size()).to(device)
-    z0 = epsilon * torch.exp(.5 * qz0_logvar) + qz0_mean
+  if args.train_dir is not None:
+    if not os.path.exists(args.train_dir):
+      os.makedirs(args.train_dir)
+    ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
+    if os.path.exists(ckpt_path):
+      checkpoint = torch.load(ckpt_path)
+      func.load_state_dict(checkpoint['func_state_dict'])
+      rec.load_state_dict(checkpoint['rec_state_dict'])
+      dec.load_state_dict(checkpoint['dec_state_dict'])
+      optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+      orig_trajs = checkpoint['orig_trajs']
+      samp_trajs = checkpoint['samp_trajs']
+      orig_ts = checkpoint['orig_ts']
+      samp_ts = checkpoint['samp_ts']
+      print('Loaded ckpt from {}'.format(ckpt_path))
 
-    # forward in time and solve ode for reconstructions
-    pred_z = odeint(func, z0, samp_ts).permute(1, 0, 2)
-    pred_x = dec(pred_z)
+  try:
+    for itr in range(1, args.niters + 1):
+      optimizer.zero_grad()
+      # backward in time to infer q(z_0)
+      h = rec.initHidden().to(device)
+      for t in reversed(range(samp_trajs.size(1))):
+        obs = samp_trajs[:, t, :]
+        out, h = rec.forward(obs, h)
+      qz0_mean, qz0_logvar = out[:, :latent_dim], out[:, latent_dim:]
+      epsilon = torch.randn(qz0_mean.size()).to(device)
+      z0 = epsilon * torch.exp(.5 * qz0_logvar) + qz0_mean
 
-    # compute loss
-    noise_std_ = torch.zeros(pred_x.size()).to(device) + noise_std
-    noise_logvar = 2. * torch.log(noise_std_).to(device)
-    logpx = log_normal_pdf(samp_trajs, pred_x, noise_logvar).sum(-1).sum(-1)
-    pz0_mean = pz0_logvar = torch.zeros(z0.size()).to(device)
-    analytic_kl = normal_kl(qz0_mean, qz0_logvar, pz0_mean, pz0_logvar).sum(-1)
-    loss = torch.mean(-logpx + analytic_kl, dim=0)
-    loss.backward()
-    optimizer.step()
-    loss_meter.update(loss.item())
+      # forward in time and solve ode for reconstructions
+      pred_z = odeint(func, z0, samp_ts).permute(1, 0, 2)
+      pred_x = dec(pred_z)
 
-    print('Iter: {}, running avg elbo: {:.4f}'.format(itr, -loss_meter.avg))
-  print("Training complete.")
+      # compute loss
+      noise_std_ = torch.zeros(pred_x.size()).to(device) + noise_std
+      noise_logvar = 2. * torch.log(noise_std_).to(device)
+      logpx = log_normal_pdf(samp_trajs, pred_x, noise_logvar).sum(-1).sum(-1)
+      pz0_mean = pz0_logvar = torch.zeros(z0.size()).to(device)
+      analytic_kl = normal_kl(qz0_mean, qz0_logvar,
+                              pz0_mean, pz0_logvar).sum(-1)
+      loss = torch.mean(-logpx + analytic_kl, dim=0)
+      loss.backward()
+      optimizer.step()
+      loss_meter.update(loss.item())
+
+      print('Iter: {}, running avg elbo: {:.4f}'.format(itr, -loss_meter.avg))
+
+  except KeyboardInterrupt:
+    if args.train_dir is not None:
+      ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
+      torch.save({
+          'func_state_dict': func.state_dict(),
+          'rec_state_dict': rec.state_dict(),
+          'dec_state_dict': dec.state_dict(),
+          'optimizer_state_dict': optimizer.state_dict(),
+          'orig_trajs': orig_trajs,
+          'samp_trajs': samp_trajs,
+          'orig_ts': orig_ts,
+          'samp_ts': samp_ts,
+      }, ckpt_path)
+      print('Stored ckpt at {}'.format(ckpt_path))
+  print('Training complete after {} iters.'.format(itr))
 
   if args.visualize:
     with torch.no_grad():
@@ -262,8 +307,9 @@ if __name__ == '__main__':
 
       # take first trajectory for visualization
       z0 = z0[0]
-      ts_pos = np.linspace(0., 3. * np.pi, num=1000)
-      ts_neg = np.linspace(-3. * np.pi, 0., num=1000)[::-1].copy()
+
+      ts_pos = np.linspace(0., 2. * np.pi, num=2000)
+      ts_neg = np.linspace(-np.pi, 0., num=2000)[::-1].copy()
       ts_pos = torch.from_numpy(ts_pos).float().to(device)
       ts_neg = torch.from_numpy(ts_neg).float().to(device)
 
@@ -271,17 +317,18 @@ if __name__ == '__main__':
       zs_neg = odeint(func, z0, ts_neg)
 
       xs_pos = dec(zs_pos)
-      xs_neg = dec(zs_neg)
+      xs_neg = torch.flip(dec(zs_neg), dims=[0])
 
-      xs = torch.cat((torch.flip(xs_neg, dims=[0]), xs_pos), dim=0)
-
-    xs = xs.cpu().numpy()
+    xs_pos = xs_pos.cpu().numpy()
+    xs_neg = xs_neg.cpu().numpy()
     orig_traj = orig_trajs[0].cpu().numpy()
     samp_traj = samp_trajs[0].cpu().numpy()
 
     plt.figure()
     plt.plot(orig_traj[:, 0], orig_traj[:, 1], 'g', label='true trajectory')
-    plt.plot(xs[:, 0], xs[:, 1], 'r', label='learned trajectory')
+    plt.plot(xs_pos[:, 0], xs_pos[:, 1], 'r', label='learned trajectory (t>0)')
+    plt.plot(xs_neg[:, 0], xs_neg[:, 1], 'c', label='learned trajectory (t<0)')
     plt.scatter(samp_traj[:, 0], samp_traj[:, 1], label='sampled data', s=3)
     plt.legend()
-    plt.savefig("./vis.png", dpi=500)
+    plt.savefig('./vis.png', dpi=500)
+    print('Saved visualization figure at {}'.format('./vis.png'))
